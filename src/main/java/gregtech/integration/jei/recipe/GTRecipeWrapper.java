c1ff819766c79cfba04d8adb0337e4000d1b495b
detav <rammirov98@gmail.com>
2021-03-04 21:08:47 -0300

    optimization

--- src/main/java/gregtech/integration/jei/recipe/GTRecipeWrapper.java
+++ src/main/java/gregtech/integration/jei/recipe/GTRecipeWrapper.java
@@ -17,7 +17,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.FluidStack;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class GTRecipeWrapper implements IRecipeWrapper {
